0952ee6926c4b53619976e6f5e8eb5971b9b595d
Michael Reuter <mareuternh@gmail.com>
2018-06-21 21:49:15 -0700

    Creating Gaussian camera and camera CCD plot.
     * Adding Gaussian camera.
     * Adding documentation to Gaussian camera class.
     * Change in function name for Gaussian camera.
     * Adding Camera Control widget.
     * Adding CameraController.
     * Adding pytest-mock requirement.
     * Changing base widgets for plots.
     * Adding pyqtgraph deps.
     * Add pytest-mock to Travis YAML.
     * First cut at camera plot integration.
     * Switching to dedicated camera plot widget.
     * Adding a status bar updater.
     * Code style renormalization.
     * Code renormalization.
     * Documentation updates.
     * More code style renormalization.
     * Fixing about typo.

--- spot_motion_monitor/utils/__init__.py
+++ spot_motion_monitor/utils/__init__.py
@@ -2,6 +2,5 @@
 # Copyright (c) 2018 LSST Systems Engineering
 # Distributed under the MIT License. See LICENSE for more information.
 #------------------------------------------------------------------------------
-from .ui_main_window import Ui_MainWindow
-from .main_window import main
-from .camera_plot_widget import CameraPlotWidget
+from .constants import *
+from .status_bar_updater import StatusBarUpdater
