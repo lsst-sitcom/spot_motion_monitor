880e1dfc0aa93f1de9334a20783c3996c7ef4497
Michael Reuter <mareuternh@gmail.com>
2018-07-11 17:49:15 -0700

    Calculating information from full CCD frame.

--- spot_motion_monitor/utils/exceptions.py
+++ spot_motion_monitor/utils/exceptions.py
@@ -2,7 +2,9 @@
 # Copyright (c) 2018 LSST Systems Engineering
 # Distributed under the MIT License. See LICENSE for more information.
 #------------------------------------------------------------------------------
-from .constants import *
-from .exceptions import *
-from .frame_information import *
-from .status_bar_updater import StatusBarUpdater
+__all__ = ["FrameRejected"]
+
+class FrameRejected(Exception):
+    """Exception for rejected frames.
+    """
+    pass
