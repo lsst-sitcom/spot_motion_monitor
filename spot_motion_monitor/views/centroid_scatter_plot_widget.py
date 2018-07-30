f196c77e289dcc49aff321915e5b806fd35efa2e
Michael Reuter <mareuternh@gmail.com>
2018-07-29 18:55:19 -0700

    Adding projection plots to scatter plot widget.

--- spot_motion_monitor/views/centroid_scatter_plot_widget.py
+++ spot_motion_monitor/views/centroid_scatter_plot_widget.py
@@ -3,28 +3,50 @@
 # Distributed under the MIT License. See LICENSE for more information.
 #------------------------------------------------------------------------------
 import numpy as np
-from pyqtgraph import GraphicsLayoutWidget, mkBrush, ScatterPlotItem
+from PyQt5.QtWidgets import QWidget
+from pyqtgraph import mkBrush, ScatterPlotItem
+
+from spot_motion_monitor.views.ui_scatter_plot import Ui_ScatterPlot
 
 __all__ = ['CentroidScatterPlotWidget']
 
-class CentroidScatterPlotWidget(GraphicsLayoutWidget):
+class CentroidScatterPlotWidget(QWidget, Ui_ScatterPlot):
 
-    """Summary
+    """This class handles managing the centroid scatter plot and the histogram
+       projections for both x and y coordinates.
 
     Attributes
     ----------
+    brushColor : tuple
+        The common color for all brushes in the widget.
+    brushes : list
+        A set of brushes with varying alpha.
     dataCounter : int
         Number of times data arrays have been appended to up until array size.
-    dataSize : TYPE
+    dataSize : int
         The requested size of the data arrays.
+    histogramFillBrush : QtGui.QBrush
+        Brush used to fill the projection histograms.
+    maxAlpha : int
+        The maximum transparency value for a brush
+    minAlpha : int
+        The minimum transparency value for a brush.
+    numBins : int
+        Number of histogram bins for the projections.
+    pointBrush : QtGui.QBrush
+        The brush for the scatter plot points.
     rollArray : bool
         Flag as to when to start rolling the data arrays of centroid values.
-    scatterPlot : TYPE
+    scatterPlotItem : pyqtgraph.ScatterPlotItem
         Instance of the centroid scatter plot.
     xData : numpy.array
         Container for the x coordinate of the centroid.
-    yData : TYPE
+    xHistogramItem : pyqtgraph.PlotItem
+        Instance of the x coordinate histogram projection.
+    yData : numpy.array
         Container for the y coordinate of the centroid.
+    yHistogramItem : pyqtgraph.PlotItem
+        Instance of the y coordinate histogram projection.
     """
 
     def __init__(self, parent=None):
@@ -36,12 +58,17 @@ class CentroidScatterPlotWidget(GraphicsLayoutWidget):
             Top-level widget.
         """
         super().__init__(parent)
+        self.setupUi(self)
 
-        p2 = self.addPlot()
-        self.scatterPlot = ScatterPlotItem()
-        p2.addItem(self.scatterPlot)
-        p2.setLabel('left', 'Y', units='pixel')
-        p2.setLabel('bottom', 'X', units='pixel')
+        p1 = self.scatterPlot.addPlot()
+        self.scatterPlotItem = ScatterPlotItem()
+        p1.addItem(self.scatterPlotItem)
+        p1.setLabel('left', 'Y', units='pixel')
+        p1.setLabel('bottom', 'X', units='pixel')
+
+        self.yHistogramItem = None
+        self.xHistogramItem = None
+        self.numBins = 40
 
         self.dataSize = None
         self.xData = None
@@ -49,9 +76,12 @@ class CentroidScatterPlotWidget(GraphicsLayoutWidget):
         self.rollArray = False
         self.dataCounter = 0
         self.brushes = None
-        self.brushColor = (100, 100, 150)
+        self.brushColor = (159, 159, 159)
         self.maxAlpha = 255
         self.minAlpha = 127
+        self.histogramFillBrush = mkBrush(*self.brushColor, 200)
+        self.pointBrush = mkBrush(*self.brushColor, self.maxAlpha)
+        self.scatterPlotItem.setBrush(self.pointBrush)
 
     def makeBrushes(self):
         """Make brushes for spots with differnet alpha factors.
@@ -77,6 +107,11 @@ class CentroidScatterPlotWidget(GraphicsLayoutWidget):
         self.xData = np.array([])
         self.yData = np.array([])
         self.makeBrushes()
+        p1 = self.yHistogram.addPlot()
+        self.yHistogramItem = p1.plot(self.yData, self.yData)
+        self.yHistogramItem.rotate(90)
+        p2 = self.xHistogram.addPlot()
+        self.xHistogramItem = p2.plot(self.xData, self.xData)
 
     def updatePlot(self, centroidX, centroidY):
         """Update the plot with a new centroid coordinate pair.
@@ -99,9 +134,19 @@ class CentroidScatterPlotWidget(GraphicsLayoutWidget):
             self.xData = np.append(self.xData, centroidX)
             self.yData = np.append(self.yData, centroidY)
             # Get brushes from end of array since that's where newer data is at.
+            # Many brushes causes performance issues.
             #brushes = self.brushes[:self.xData.size]
 
-        self.scatterPlot.setData(self.xData, self.yData, pen=None)  # , brush=brushes)
+        self.scatterPlotItem.setData(self.xData, self.yData, pen=None)  # , brush=brushes)
+
+        xy, xx = np.histogram(self.xData,
+                              bins=np.linspace(np.min(self.xData), np.max(self.xData), self.numBins))
+        self.xHistogramItem.setData(xx, xy, stepMode=True, fillLevel=0, fillBrush=self.histogramFillBrush)
+        yy, yx = np.histogram(self.yData,
+                              bins=np.linspace(np.min(self.yData), np.max(self.yData), self.numBins))
+        # Flip due to rotated plot
+        yy *= -1
+        self.yHistogramItem.setData(yx, yy, stepMode=True, fillLevel=0, fillBrush=self.histogramFillBrush)
 
         if self.dataCounter < self.dataSize:
             self.dataCounter += 1
--- tests/views/test_centroid_scatter_plot_widget.py
+++ tests/views/test_centroid_scatter_plot_widget.py
@@ -6,20 +6,30 @@ from spot_motion_monitor.views import CentroidScatterPlotWidget
 
 class TestCentroidScatterPlotWidget:
 
+    def makeColorTuple(self, obj):
+        return (obj.color().red(), obj.color().blue(), obj.color().green())
+
     def test_parametersAfterConstruction(self, qtbot):
         cspw = CentroidScatterPlotWidget()
-        cspw.show()
         qtbot.addWidget(cspw)
         assert cspw.scatterPlot is not None
         assert cspw.dataSize is None
         assert cspw.xData is None
         assert cspw.yData is None
+        assert cspw.xHistogramItem is None
+        assert cspw.yHistogramItem is None
+        assert cspw.numBins == 40
         assert cspw.rollArray is False
         assert cspw.dataCounter == 0
         assert cspw.brushes is None
-        assert cspw.brushColor == (100, 100, 150)
+        color = (159, 159, 159)
+        assert cspw.brushColor == color
         assert cspw.maxAlpha == 255
         assert cspw.minAlpha == 127
+        hFillBrush = self.makeColorTuple(cspw.histogramFillBrush)
+        assert hFillBrush == color
+        pBrush = self.makeColorTuple(cspw.pointBrush)
+        assert pBrush == color
 
     def test_parametersAfterSetup(self, qtbot):
         cspw = CentroidScatterPlotWidget()
@@ -30,6 +40,8 @@ class TestCentroidScatterPlotWidget:
         assert cspw.dataSize == arraySize
         assert cspw.xData is not None
         assert cspw.yData is not None
+        assert cspw.xHistogramItem is not None
+        assert cspw.yHistogramItem is not None
         assert cspw.rollArray is False
         assert cspw.dataCounter == 0
         assert cspw.brushes is not None
@@ -43,14 +55,13 @@ class TestCentroidScatterPlotWidget:
 
     def test_updatePlot(self, qtbot, mocker):
         cspw = CentroidScatterPlotWidget()
-        cspw.show()
         qtbot.addWidget(cspw)
         arraySize = 3
         cspw.setup(arraySize)
         truthAlpha = [127, 191, 255]
         alpha = [x.color().alpha() for x in cspw.brushes]
         assert alpha == truthAlpha
-        mockSetData = mocker.patch.object(cspw.scatterPlot, 'setData')
+        mockSetData = mocker.patch.object(cspw.scatterPlotItem, 'setData')
         valuesX = [254.43, 254.86, 253.91, 254.21]
         valuesY = [355.25, 355.10, 354.89, 355.57]
         cspw.updatePlot(valuesX[0], valuesY[0])
