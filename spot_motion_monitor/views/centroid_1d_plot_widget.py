4abcbcd3c1a49b5763b4d06a26174ea18b48abb9
Michael Reuter <mareuternh@gmail.com>
2018-09-02 17:28:34 -0700

    Making 1D centroid plots disable auto range after start.

--- spot_motion_monitor/views/centroid_1d_plot_widget.py
+++ spot_motion_monitor/views/centroid_1d_plot_widget.py
@@ -14,18 +14,28 @@ class Centroid1dPlotWidget(GraphicsLayoutWidget):
 
     Attributes
     ----------
-    curve : pyqtgraph.PlotItem
-        Instance of the line plot.
+    curve : pyqtgraph.PlotDataItem
+        Instance of the line in the plot.
     data : numpy.array
         Container for the centroid data.
     dataCounter : int
         Number of times data array has been appended to up until array size.
     dataSize : int
         The requested size of the data array.
+    numAccumFrames : int
+        The number of frames to accumulate before calculating y range.
+    pixelRangeAddition : int
+        The value to subtract and add to the mean of the accumulated data.
+    plot : pyqtgraph.PlotItem
+        Instance of the graphics plot.
     roiFps : float
         The camera ROI FPS.
     rollArray : bool
         Flag as to when to start rolling the data array of centroid values.
+    timeRange : numpy.array
+        The values for the accumulation time range.
+    yRange : list
+        The bounds for the y axis of the plot when disabling auto range.
     """
 
     def __init__(self, parent=None):
@@ -37,6 +47,7 @@ class Centroid1dPlotWidget(GraphicsLayoutWidget):
             Top-level widget.
         """
         super().__init__(parent)
+        self.plot = None
         self.curve = None
         self.dataSize = None
         self.data = None
@@ -44,6 +55,9 @@ class Centroid1dPlotWidget(GraphicsLayoutWidget):
         self.rollArray = False
         self.dataCounter = 0
         self.roiFps = None
+        self.yRange = None
+        self.pixelRangeAddition = 5
+        self.numAccumFrames = 15
 
     def setup(self, arraySize, axisLabel, roiFps):
         """Provide information for setting up the plot.
@@ -58,13 +72,13 @@ class Centroid1dPlotWidget(GraphicsLayoutWidget):
             The camera ROI FPS.
         """
         self.dataSize = arraySize
-        self.data = np.array([])
-        self.timeRange = np.array([])
+        self.data = np.zeros(self.dataSize)
         self.roiFps = roiFps
-        p1 = self.addPlot()
-        self.curve = p1.plot(self.data)
-        p1.setLabel('bottom', 'Time', units='s')
-        p1.setLabel('left', axisLabel, units='pixel')
+        self.timeRange = np.arange(self.dataSize) / self.roiFps
+        self.plot = self.addPlot()
+        self.curve = self.plot.plot(self.timeRange, self.data)
+        self.plot.setLabel('bottom', 'Time', units='s')
+        self.plot.setLabel('left', axisLabel, units='pixel')
 
     def updatePlot(self, centroid):
         """Update the plot with a new centroid.
@@ -78,15 +92,17 @@ class Centroid1dPlotWidget(GraphicsLayoutWidget):
             self.data[:-1] = self.data[1:]
             self.data[-1] = centroid
         else:
-            # This does create copies of arrays, so watch performance.
-            self.data = np.append(self.data, centroid)
-            self.timeRange = np.arange(self.data.size) / self.roiFps
-
-        self.curve.setData(self.timeRange, self.data)
-        if self.data.size > 0.1 * self.dataSize:
-            self.curve.setDownsampling('mean')
+            self.data[self.dataCounter] = centroid
 
         if self.dataCounter < self.dataSize:
             self.dataCounter += 1
             if self.dataCounter == self.dataSize:
                 self.rollArray = True
+
+        if self.dataCounter == self.numAccumFrames and self.yRange is None:
+            cmean = int(np.mean(self.data[0:self.numAccumFrames]))
+            self.yRange = [cmean - self.pixelRangeAddition, cmean + self.pixelRangeAddition]
+            self.plot.setRange(yRange=self.yRange)
+            self.plot.disableAutoRange()
+
+        self.curve.setData(self.timeRange, self.data)
--- tests/views/test_centroid_1d_widget.py
+++ tests/views/test_centroid_1d_widget.py
@@ -9,6 +9,7 @@ class TestCentroid1dPlotWidget():
     def test_parametersAfterConstruction(self, qtbot):
         c1dpw = Centroid1dPlotWidget()
         qtbot.addWidget(c1dpw)
+        assert c1dpw.plot is None
         assert c1dpw.curve is None
         assert c1dpw.dataSize is None
         assert c1dpw.data is None
@@ -16,6 +17,9 @@ class TestCentroid1dPlotWidget():
         assert c1dpw.dataCounter == 0
         assert c1dpw.rollArray is False
         assert c1dpw.roiFps is None
+        assert c1dpw.yRange is None
+        assert c1dpw.pixelRangeAddition == 5
+        assert c1dpw.numAccumFrames == 15
 
     def test_parametersAfterSetup(self, qtbot):
         c1dpw = Centroid1dPlotWidget()
@@ -23,10 +27,11 @@ class TestCentroid1dPlotWidget():
         arraySize = 1000
         roiFps = 40
         c1dpw.setup(arraySize, 'X', roiFps)
+        assert c1dpw.plot is not None
         assert c1dpw.curve is not None
-        assert c1dpw.dataSize is arraySize
-        assert c1dpw.data is not None
-        assert c1dpw.timeRange is not None
+        assert c1dpw.dataSize == arraySize
+        assert c1dpw.data.size == arraySize
+        assert c1dpw.timeRange.size == arraySize
         assert c1dpw.rollArray is False
         assert c1dpw.roiFps == roiFps
 
@@ -36,23 +41,25 @@ class TestCentroid1dPlotWidget():
         qtbot.addWidget(c1dpw)
         arraySize = 3
         currentFps = 2
+        c1dpw.numAccumFrames = 2
         c1dpw.setup(arraySize, 'X', currentFps)
+        timeRange = [0.0, 0.5, 1.0]
+        assert c1dpw.timeRange.tolist() == timeRange
         mockSetData = mocker.patch.object(c1dpw.curve, 'setData')
         values = [254.43, 254.86, 253.91, 254.21]
-        timeRange = [0.0, 0.5, 1.0]
         c1dpw.updatePlot(values[0])
-        assert c1dpw.data.tolist() == [values[0]]
+        assert c1dpw.data.tolist() == [values[0]] + 2 * [0.0]
         assert c1dpw.dataCounter == 1
-        assert c1dpw.timeRange.tolist() == [0]
+        assert c1dpw.yRange is None
         c1dpw.updatePlot(values[1])
+        assert c1dpw.data.tolist() == [values[0], values[1], 0.0]
+        assert c1dpw.yRange == [249, 259]
         c1dpw.updatePlot(values[2])
         assert c1dpw.data.tolist() == values[:-1]
         assert c1dpw.dataCounter == arraySize
         assert c1dpw.rollArray is True
-        assert c1dpw.timeRange.tolist() == timeRange
         c1dpw.updatePlot(values[3])
         assert c1dpw.data.tolist() == values[1:]
         assert c1dpw.dataCounter == arraySize
         assert c1dpw.rollArray is True
-        assert c1dpw.timeRange.tolist() == timeRange
         assert mockSetData.call_count == len(values)
