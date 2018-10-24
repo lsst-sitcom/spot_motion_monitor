a4287cdd9e7e1cb05665c087b48cbc1d39eaf28f
Michael Reuter <mareuternh@gmail.com>
2018-10-24 16:52:02 -0700

    Changing version number before tagging.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ test_requirements = [
 
 setup(
     name='spot_motion_monitor',
-    version='0.7.0',
+    version='0.8.0',
     description='User interface for Spot Seeing Monitor.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Michael Reuter',
