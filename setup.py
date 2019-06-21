a8a91fb7f3bcdf65bc7458ca70f893013cdfce14
Michael Reuter <mareuter@lsst.org>
2019-06-20 17:40:40 -0700

    Adding certification for Python 3.7.

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     cmdclass=cmdclass,
     test_suite='tests',
