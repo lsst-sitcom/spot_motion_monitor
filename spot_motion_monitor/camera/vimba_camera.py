2ffa710ab840cd45da19e0bca71d111938808cea
Michael Reuter <mareuter@lsst.org>
2019-10-23 17:29:49 -0700

    Clean up Vimba camera code.

--- spot_motion_monitor/camera/vimba_camera.py
+++ spot_motion_monitor/camera/vimba_camera.py
@@ -3,7 +3,7 @@
 # Distributed under the MIT License. See LICENSE for more information.
 #------------------------------------------------------------------------------
 from datetime import datetime
-# import time
+import time
 
 import numpy as np
 import pymba as pv
@@ -155,22 +155,11 @@ class VimbaCamera(BaseCamera):
             Raises this if the camera fails to capture the frame when
             requested.
         """
-        #try:
-        #    self.frame.queue_for_capture()
-        #except pv.VimbaException as err:
-        #    raise FrameCaptureFailed("{} Full frame capture failed: {}".format(datetime.now(), str(err)))
-
-        #self.cameraPtr.run_feature_command('AcquisitionStart')
         try:
             self.cameraPtr.start_frame_acquisition()
         except pv.VimbaException as err:
             raise FrameCaptureFailed("{} Full frame capture failed: {}".format(datetime.now(), str(err)))
-        #self.frame = self.cameraPtr.acquire_frame()
-        #self.cameraPtr.run_feature_command('AcquisitionStop')
-        #self.frame.wait_for_capture(1000)
-        #frameData = self.frame.buffer_data()
 
-        #img = np.ndarray(buffer=frameData, dtype=np.uint16, shape=(self.height, self.width))
         return self.image
 
     def getOffset(self):
@@ -198,28 +187,15 @@ class VimbaCamera(BaseCamera):
             requested.
         """
         self.totalFrames += 1
-        #try:
-        #    self.frame.queue_for_capture()
-        #except pv.VimbaException as err:
-        #    self.badFrames += 1
-        #    raise FrameCaptureFailed("{} ROI frame capture failed: {}".format(getTimestamp(), str(err)))
         try:
             self.cameraPtr.start_frame_acquisition()
             self.goodFrames += 1
         except pv.VimbaException as err:
             self.badFrames += 1
             raise FrameCaptureFailed("{} ROI frame capture failed: {}".format(getTimestamp(), str(err)))
-        #self.cameraPtr.run_feature_command('AcquisitionStart')
-        #self.cameraPtr.run_feature_command('AcquisitionStop')
-        #try:
-        #    self.frame.wait_for_capture(1)
-        #except pv.VimbaException as err:
-        #    self.goodFrames -= 1
-        #    self.badFrames += 1
-        #    raise FrameCaptureFailed("{} ROI frame capture wait failed: {}".format(getTimestamp(), str(err)))
-        #frameData = self.frame.buffer_data()
+
         self.cameraPtr.stop_frame_acquisition()
-        #img = np.ndarray(buffer=frameData, dtype=np.uint16, shape=(self.roiSize, self.roiSize))
+
         return self.image
 
     def resetOffset(self):
@@ -274,9 +250,7 @@ class VimbaCamera(BaseCamera):
         self.totalFrames = 0
         self.vimba = pv.Vimba()
         self.vimba.startup()
-        #system = self.vimba.system()
-        #system.run_feature_command('GeVDiscoveryAllOnce')
-        #time.sleep(0.2)
+        time.sleep(0.2)
         cameraIds = self.vimba.camera_ids()
         try:
             self.cameraPtr = self.vimba.camera(cameraIds[self.cameraIndex])
@@ -293,17 +267,12 @@ class VimbaCamera(BaseCamera):
         self.cameraPtr.OffsetX = 0
         self.cameraPtr.OffsetY = 0
         self.cameraPtr.GainAuto = 'Off'
-        #self.cameraPtr.GainRaw = 0
-        self.cameraPtr.feature('GainRaw').value = 0
+        self.cameraPtr.GainRaw = 0
         self.cameraPtr.ExposureAuto = 'Off'
-        #self.cameraPtr.AcquisitionMode = 'Continuous'
         self.cameraPtr.TriggerSource = 'Freerun'
         self.cameraPtr.PixelFormat = 'Mono12'
         self.cameraPtr.ExposureTimeAbs = self.roiExposureTime
 
-        #self.frame = self.cameraPtr.new_frame()
-        #self.frame.announce()
-        #self.cameraPtr.start_capture()
         self.cameraPtr.arm('Continuous', self.convertFrame)
 
     def shutdown(self):
@@ -311,8 +280,6 @@ class VimbaCamera(BaseCamera):
         """
         if self.cameraPtr is not None:
             try:
-                #self.cameraPtr.end_capture()
-                #self.cameraPtr.revoke_all_frames()
                 self.cameraPtr.disarm()
                 self.cameraPtr.close()
             except pv.VimbaException:
