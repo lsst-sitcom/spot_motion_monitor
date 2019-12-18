7196ce0437094556878bb5600b37bd84b2eaa283
Michael Reuter <mareuter@lsst.org>
2019-12-18 11:24:51 -0700

    Adjust size of camera info dialog based on content.

--- spot_motion_monitor/views/camera_information_dialog.py
+++ spot_motion_monitor/views/camera_information_dialog.py
@@ -4,6 +4,7 @@
 #------------------------------------------------------------------------------
 import os
 
+from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtWidgets import QDialog
 
 from .forms.ui_camera_info_dialog import Ui_CameraInformationDialog
@@ -36,7 +37,24 @@ class CameraInformationDialog(QDialog, Ui_CameraInformationDialog):
             The set of camera information to display.
         """
         lines = []
+        max_width = 0
+        max_height = len(cameraInfo.keys())
         for key, value in cameraInfo.items():
             lines.append(f'<p><b>{key}</b>: {value}</p>')
+            max_width = max(max_width, len(key) + len(str(value)) + 2)
+
+        fontMetrics = QFontMetrics(self.cameraInfoTextBrowser.font())
+
+        scale_factor = 1.75
+
+        browser_height = int(max_height * fontMetrics.lineSpacing() * scale_factor)
+        browser_width = int((max_width * fontMetrics.maxWidth()) / scale_factor)
 
         self.cameraInfoTextBrowser.setHtml(os.linesep.join(lines))
+
+        if self.cameraInfoTextBrowser.minimumHeight() < browser_height:
+            self.cameraInfoTextBrowser.setMinimumHeight(browser_height)
+            self.cameraInfoTextBrowser.setMaximumHeight(browser_height)
+        if self.cameraInfoTextBrowser.minimumWidth() < browser_width:
+            self.cameraInfoTextBrowser.setMinimumWidth(browser_width)
+            self.cameraInfoTextBrowser.setMaximumWidth(browser_width)
