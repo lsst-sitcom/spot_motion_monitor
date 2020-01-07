552003e420424ba1e004d5f4c4cfa712ece29e5d
Michael Reuter <mareuter@lsst.org>
2020-01-07 15:15:37 -0700

    Change license header is source files.

--- spot_motion_monitor/config/psd_plot_config.py
+++ spot_motion_monitor/config/psd_plot_config.py
@@ -1,7 +1,13 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2018-2019 LSST Systems Engineering
-# Distributed under the MIT License. See LICENSE for more information.
-#------------------------------------------------------------------------------
+# This file is part of spot_motion_monitor.
+#
+# Developed for LSST System Integration, Test and Commissioning.
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 from spot_motion_monitor.config import BaseConfig
 
 __all__ = ['PsdPlotConfig']
